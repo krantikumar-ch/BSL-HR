b4135740a5cc16d518db0e901e4e08ec8f551b63
Kranti <krantikumar.ch@gmail.com>
2020-04-20 15:02:29 +0530

    Added ResponseBodyEmitter for client-server stream

--- src/main/java/com/example/SpringBootJPA/controller/EmployeeController.java
+++ src/main/java/com/example/SpringBootJPA/controller/EmployeeController.java
@@ -1,6 +1,9 @@
 package com.example.SpringBootJPA.controller;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -10,6 +13,8 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyEmitter;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import com.example.SpringBootJPA.entities.EmployeeEntity;
 import com.example.SpringBootJPA.service.EmployeeService;
@@ -44,5 +49,37 @@ public class EmployeeController {
 	@GetMapping("getEmployee")
 	public EmployeeEntity getEmployee(@RequestParam("employeeId") Long empId){
 		return empService.getEmployee(empId);
+	}
+	
+	@GetMapping("getEmployeesByStream")
+	public ResponseBodyEmitter getEmployeesByStream(){
+		ResponseBodyEmitter emitter = new ResponseBodyEmitter();
+		List<EmployeeEntity> empEntities = empService.getAllEmployees();
+		ExecutorService executor = Executors.newSingleThreadExecutor();
+		List<EmployeeEntity> subList = new ArrayList<>();
+		executor.execute(() ->{
+			try{
+				
+				for(EmployeeEntity employeeEntity : empEntities){
+					if(subList.size() ==5){
+						emitter.send(subList);
+						subList.clear();
+						Thread.sleep(2000);
+					}
+					subList.add(employeeEntity);
+					
+				}
+				if(subList.size() !=0){
+					emitter.send(subList);
+				}
+				emitter.complete();
+			}
+			catch(Exception e){
+				e.printStackTrace();
+				emitter.completeWithError(e);
+			}
+		});
+		executor.shutdown();
+		return emitter;
 	}
 }
